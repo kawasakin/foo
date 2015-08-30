982cfd2ec87c64348f85f85eea2721c6e3d8a051
Rongxin Fang <r3fang@ucsd.edu>
2015-08-29 19:39:52 -0700

    add results

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# foo
+# Author: Rongxin Fang
+# E-mail: r3fang@ucsd.edu
+# Created Date: 06-21-2015
+# Last Modified Date: 07-01-2015
+# Last Modified Server: Silencer
+# LICENSE: Ren Lab (http://bioinformatics-renlab.ucsd.edu/rentrac/)
+# Project Name: Enhancer-Promoter Targeting (EPT)
+# Description: Characterization of putative enhancer–promoter targeting.
---
+++ results/07-01-2015/notebook.sh
@@ -0,0 +1,67 @@
+#PBS -q hotel 
+#PBS -N HTseq_count
+#PBS -l nodes=5:ppn=4
+#PBS -l walltime=70:00:00
+#PBS -o HTseq_count.out
+#PBS -e HTseq_count.err
+#PBS -V
+#PBS -M r3fang@ucsd.edu
+#PBS -m ae
+#PBS -A ren-group
+###### qsub -I -q hotel -N GeneExp -l nodes=6:ppn=2 -l walltime=100:00:00 -A ren-group 
+
+# constant
+RNA_SEQ_DIR="/oasis/tscc/scratch/r3fang/data/Mus_musculus/UCSC/mm9/ENCODE/RNA-seq/"
+GENOME="/oasis/tscc/scratch/r3fang/data/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/genome.fa"
+GTF="/oasis/tscc/scratch/r3fang/data/Mus_musculus/UCSC/mm9/Annotation/Genes/genes.gtf"
+
+cd /oasis/tscc/scratch/r3fang/github/foo/results/07-01-2015/
+
+#0. delete all files under current folder with size 0
+find . -size  0 -print0 | xargs -0 rm
+
+#1. Read counting 
+files=`ls $RNA_SEQ_DIR`
+for name in $files
+do
+	if [[ $name == *".bam" ]]
+	then
+		if [ ! -f "${name%.bam}.count" ]
+			then
+				samtools view -h $RNA_SEQ_DIR/$name | python2.7 -m HTSeq.scripts.count -m union - $GTF > "${name%.bam}.count" &
+			fi
+	fi
+done
+
+#2. convert count to CPM
+#files=`ls .`
+#for name in $files 
+#do
+#	if [ -s "$name" ]
+#	then
+#		if [[ $name == *".count" ]]
+#		then
+#			echo $name
+#			cat $name | python ../../bin/CPM.py - > "${name%.count}.CPM"
+#		fi
+#	fi
+#done
+
+#3. normailize by R
+#ls | grep CPM | grep -v name > CPM_name.txt 
+# R
+#name <- read.table('CPM_name.txt')
+#data <- read.table(as.character(name[1,]))
+#rownames(data) <- data[,1]
+#for(i in 2:nrow(name)){
+#	item <- name[i,]
+#	tmp <- read.table(as.character(item))
+#	data <- cbind(data, tmp[,2])
+#}
+#data <- data[,-1]
+#a <- apply(data, 1, var)
+
+#write.table(data.frame(a[order(a)]), file = "tmp.txt", append = FALSE, quote = FALSE, sep = "\t",
+#                 eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+#                 col.names = FALSE, qmethod = c("escape", "double"),
+#                 fileEncoding = "")
