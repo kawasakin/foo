4fda81bd802eb72b5dc7cdd11023f9045bf7f04e
Rongxin Fang <r3fang@ucsd.edu>
2015-08-30 11:29:24 -0700

    add gene_cluster.RData

--- results/07-01-2015/notebook.sh
+++ results/07-01-2015/notebook.sh
@@ -8,7 +8,7 @@
 #PBS -M r3fang@ucsd.edu
 #PBS -m ae
 #PBS -A ren-group
-###### qsub -I -q hotel -N GeneExp -l nodes=6:ppn=2 -l walltime=100:00:00 -A ren-group 
+###### qsub -I -q hotel -N GeneExp -l nodes=1:ppn=1 -l walltime=100:00:00 -A ren-group 
 
 # constant
 RNA_SEQ_DIR="/oasis/tscc/scratch/r3fang/data/Mus_musculus/UCSC/mm9/ENCODE/RNA-seq/"
@@ -49,33 +49,38 @@ cd /oasis/tscc/scratch/r3fang/github/foo/results/07-01-2015/
 
 #3. normailization 
 # get the name of all .CPM files
-ls | grep CPM | grep -v name > CPM_name.txt 
+# ls | grep CPM | grep -v name > CPM_name.txt 
 
 # R
-name <- read.table('CPM_name.txt')
-data <- read.table(as.character(name[1,]))
-rownames(data) <- data[,1]
-for(i in 2:nrow(name)){
-	item <- name[i,]
-	tmp <- read.table(as.character(item))
-	data <- cbind(data, tmp[,2])
-}
-data <- data[,-1]
-sd_rows <- apply(data, 1, sd)
-mean_rows <- apply(data, 1, mean)
+ name <- read.table('CPM_name.txt')
+ data <- read.table(as.character(name[1,]))
+ rownames(data) <- data[,1]
+ for(i in 2:nrow(name)){
+ 	item <- name[i,]
+ 	tmp <- read.table(as.character(item))
+ 	data <- cbind(data, tmp[,2])
+ }
+ data <- data[,-1]
+ # calculate mean and standard variance for each gene
+ sd_rows <- apply(data, 1, sd)
+ sd_rows[which(is.na(sd_rows))] = 0
+ data <- data[which(sd_rows>5),]
+ 
+ sd_rows <- apply(data, 1, sd)
+ mean_rows <- apply(data, 1, mean)
+ 
+ RET=data.frame()
+ for(i in 1:nrow(data)){
+ 	print(i);
+ 	RET = rbind(RET,(data[i,] - mean_rows[i])/sd_rows[i])
+ }
+ 
+ write.table(RET, file = "CPM_norm.txt", append = FALSE, quote = FALSE, sep = "\t",
+                  eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+                  col.names = FALSE, qmethod = c("escape", "double"),
+                  fileEncoding = "")
 
-RET=data.frame()
-for(i in 1:length(mean_rows)){
-	print(i);
-	RET = rbind(RET,(data[i,] - mean_rows[i])/sd_rows[i])
-}
-
-write.table(RET, file = "CPM_norm.txt", append = FALSE, quote = FALSE, sep = "\t",
-                 eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-                 col.names = FALSE, qmethod = c("escape", "double"),
-                 fileEncoding = "")
+save.image(file="gene_cluster.RData")
 
 # 4. cluster genes
-data = read.table("CPM_norm.txt.gz", row.names=1)
-d <- dist(as.matrix(data))
 
