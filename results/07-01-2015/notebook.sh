5f6dfcf9605686803547c818a4ffbfe0e19db5cd
Rongxin Fang <r3fang@ucsd.edu>
2015-08-30 10:12:17 -0700

    add CPM and CPM_norm

--- results/07-01-2015/notebook.sh
+++ results/07-01-2015/notebook.sh
@@ -21,47 +21,61 @@ cd /oasis/tscc/scratch/r3fang/github/foo/results/07-01-2015/
 # find . -size  0 -print0 | xargs -0 rm
 
 #1. counting expression of genes counting 
-files=`ls $RNA_SEQ_DIR`
-for name in $files
-do
-	if [[ $name == *".bam" ]]
-	then
-		if [ ! -f "${name%.bam}.count" ]
-			then
-				samtools view -h $RNA_SEQ_DIR/$name | python2.7 -m HTSeq.scripts.count -m union - $GTF > "${name%.bam}.count" &
-			fi
-	fi
-done
+# files=`ls $RNA_SEQ_DIR`
+# for name in $files
+# do
+# 	if [[ $name == *".bam" ]]
+# 	then
+# 		if [ ! -f "${name%.bam}.count" ]
+# 			then
+# 				samtools view -h $RNA_SEQ_DIR/$name | python2.7 -m HTSeq.scripts.count -m union - $GTF > "${name%.bam}.count" &
+# 			fi
+# 	fi
+# done
 
 #2. convert count to CPM
-#files=`ls .`
-#for name in $files 
-#do
-#	if [ -s "$name" ]
-#	then
-#		if [[ $name == *".count" ]]
-#		then
-#			echo $name
-#			cat $name | python ../../bin/CPM.py - > "${name%.count}.CPM"
-#		fi
-#	fi
-#done
+# files=`ls .`
+# for name in $files 
+# do
+# 	if [ -s "$name" ]
+# 	then
+# 		if [[ $name == *".count" ]]
+# 		then
+# 			echo $name
+# 			cat $name | python ../../bin/CPM.py - > "${name%.count}.CPM"
+# 		fi
+# 	fi
+# done
 
-#3. normailize by R
-#ls | grep CPM | grep -v name > CPM_name.txt 
+#3. normailization 
+# get the name of all .CPM files
+ls | grep CPM | grep -v name > CPM_name.txt 
+
 # R
-#name <- read.table('CPM_name.txt')
-#data <- read.table(as.character(name[1,]))
-#rownames(data) <- data[,1]
-#for(i in 2:nrow(name)){
-#	item <- name[i,]
-#	tmp <- read.table(as.character(item))
-#	data <- cbind(data, tmp[,2])
-#}
-#data <- data[,-1]
-#a <- apply(data, 1, var)
+name <- read.table('CPM_name.txt')
+data <- read.table(as.character(name[1,]))
+rownames(data) <- data[,1]
+for(i in 2:nrow(name)){
+	item <- name[i,]
+	tmp <- read.table(as.character(item))
+	data <- cbind(data, tmp[,2])
+}
+data <- data[,-1]
+sd_rows <- apply(data, 1, sd)
+mean_rows <- apply(data, 1, mean)
 
-#write.table(data.frame(a[order(a)]), file = "tmp.txt", append = FALSE, quote = FALSE, sep = "\t",
-#                 eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-#                 col.names = FALSE, qmethod = c("escape", "double"),
-#                 fileEncoding = "")
+RET=data.frame()
+for(i in 1:length(mean_rows)){
+	print(i);
+	RET = rbind(RET,(data[i,] - mean_rows[i])/sd_rows[i])
+}
+
+write.table(RET, file = "CPM_norm.txt", append = FALSE, quote = FALSE, sep = "\t",
+                 eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+                 col.names = FALSE, qmethod = c("escape", "double"),
+                 fileEncoding = "")
+
+# 4. cluster genes
+data = read.table("CPM_norm.txt.gz", row.names=1)
+d <- dist(as.matrix(data))
+
