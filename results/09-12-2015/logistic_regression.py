ccd1a8754bfacba5cd30259abe00ce7278172377
Rongxin Fang <r3fang@ucsd.edu>
2015-09-17 20:06:19 -0700

    modern_net.py

--- results/09-12-2015/logistic_regression.py
+++ results/09-12-2015/logistic_regression.py
@@ -4,15 +4,6 @@ import numpy as np
 from StringIO import StringIO 
 import random 
 
-def load_data(dataset, n):
-    with open(dataset) as fin:
-        dat = np.loadtxt(StringIO(fin.read()), dtype="float32")    
-    dat_X = dat[:,:(dat.shape[1]-1)]    
-    dat_Y = np.array([ [0]*(x-1) + [1] + [0]*(n-x) for x in list(dat[:,dat.shape[1]-1])])
-    train_index = random.sample(xrange(dat.shape[0]), dat.shape[0]*4/5)
-    test_index  = sorted(list(set(range(dat.shape[0]))-set(train_index)))
-    return [dat_X[train_index,], dat_Y[train_index,], dat_X[test_index,], dat_Y[test_index,]]
-
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
 
@@ -22,8 +13,32 @@ def init_weights(shape):
 def model(X, w):
     return T.nnet.softmax(T.dot(X, w))
 
-n = 2
-trX, trY, teX, teY = load_data("mESC_sample_features.txt", n)
+def load_data(dataset, num_cases, n):
+    with open(dataset) as fin:
+        dat = np.loadtxt(StringIO(fin.read()), dtype="float32")    
+    dat = dat[range(num_cases/2) + range((dat.shape[0]-num_cases/2), dat.shape[0]), ]  
+    dat_X = dat[:,:(dat.shape[1]-1)]    
+    dat_Y = dat[:,dat.shape[1]-1]
+    i = int(max(dat_Y))+1
+    for p in [min(dat_Y)-0.1] + [np.percentile(dat_Y, x*100) for x in np.arange(0, 1, 1/float(n))[1:]] + [max(dat_Y)+0.1]:
+        dat_Y[dat_Y <= p] = i
+        i += 1 
+    dat_Y = dat_Y - min(dat_Y) + 1
+    dat_Y = np.array([ [0]*(x-1) + [1] + [0]*(n-x) for x in list(dat_Y)])
+    train_index = random.sample(xrange(dat.shape[0]), dat.shape[0]*4/5)
+    test_index  = sorted(list(set(range(dat.shape[0]))-set(train_index)))
+    return [dat_X[train_index,], dat_Y[train_index,], dat_X[test_index,], dat_Y[test_index,]]
+    
+    
+    
+dataset = "mESC-zy27.gene.expr.sel.feat"
+num_cases = 2000
+num_class = 2
+sigma = 0.05 # learning rate
+epos = 200
+min_batch = 500
+
+trX, trY, teX, teY = load_data(dataset, num_cases, num_class)
 
 X = T.fmatrix()
 Y = T.fmatrix()
@@ -35,13 +50,13 @@ y_pred = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
 gradient = T.grad(cost=cost, wrt=w)
-update = [[w, w - gradient * 0.05]]
+update = [[w, w - gradient * sigma]]
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=update, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=py_x, allow_input_downcast=True)
 
-for i in range(100):
-    for start, end in zip(range(0, len(trX), 500), range(500, len(trX), 500)):
+for i in range(epos):
+    for start, end in zip(range(0, len(trX), min_batch), range(min_batch, len(trX), min_batch)):
         cost = train(trX[start:end], trY[start:end])
     print np.mean(np.argmax(teY, axis=1) == np.argmax(predict(teX), axis=1))    
 
@@ -49,10 +64,9 @@ for i in range(100):
 pred = predict(teX)
 print np.mean(np.argmax(teY, axis=1) == np.argmax(predict(teX), axis=1))    
 
-pred[,2] = np.argmax(teY, axis=1)
-np.append(pred, np.argmax(teY, axis=1), 1)
 
-np.savetxt("tmp.txt", data_Y)
+
+np.savetxt("tmp.txt", pred)
 #preds = predict(teX)
 #for i in xrange(len(predict(teX))):
 #    print preds[i], np.argmax(teY, axis=1)[i]
--- results/09-12-2015/modern_net.py
+++ results/09-12-2015/modern_net.py
@@ -7,18 +7,6 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 srng = RandomStreams()
 
-def load_data(dataset):
-    with open(dataset) as fin:
-        dat = np.loadtxt(StringIO(fin.read()), dtype="float32")
-    
-    dat_X = dat[:,:(dat.shape[1]-1)]        
-    dat_Y = np.array([[1, 0] if x==0 else [0, 1] for x in list(dat[:,dat.shape[1]-1])])
-    
-    train_index = random.sample(xrange(dat.shape[0]), dat.shape[0]*4/5)
-    test_index = list(set(range(dat.shape[0]))-set(train_index))    
-    
-    return [dat_X[train_index,], dat_Y[train_index,], dat_X[test_index,], dat_Y[test_index,]]
-
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
 
@@ -44,6 +32,22 @@ def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
         updates.append((p, p - lr * g))
     return updates
 
+def load_data(dataset, num_cases, n):
+    with open(dataset) as fin:
+        dat = np.loadtxt(StringIO(fin.read()), dtype="float32")    
+    dat = dat[range(num_cases/2) + range((dat.shape[0]-num_cases/2), dat.shape[0]), ]  
+    dat_X = dat[:,:(dat.shape[1]-1)]    
+    dat_Y = dat[:,dat.shape[1]-1]
+    i = int(max(dat_Y))+1
+    for p in [min(dat_Y)-0.1] + [np.percentile(dat_Y, x*100) for x in np.arange(0, 1, 1/float(n))[1:]] + [max(dat_Y)+0.1]:
+        dat_Y[dat_Y <= p] = i
+        i += 1 
+    dat_Y = dat_Y - min(dat_Y) + 1
+    dat_Y = np.array([ [0]*(x-1) + [1] + [0]*(n-x) for x in list(dat_Y)])
+    train_index = random.sample(xrange(dat.shape[0]), dat.shape[0]*4/5)
+    test_index  = sorted(list(set(range(dat.shape[0]))-set(train_index)))
+    return [dat_X[train_index,], dat_Y[train_index,], dat_X[test_index,], dat_Y[test_index,]]
+
 def dropout(X, p=0.):
     if p > 0:
         retain_prob = 1 - p
@@ -62,17 +66,21 @@ def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
     py_x = softmax(T.dot(h2, w_o))
     return h, h2, py_x
 
-trX, trY, teX, teY = load_data("mESC_sample_features.txt")
+dataset = "mESC-zy27.gene.expr.sel.feat"
+num_cases = 10000
+num_class = 2
+lr = 0.005 # learning rate
+epos = 200
+min_batch = 400
+
+trX, trY, teX, teY = load_data(dataset, num_cases, num_class)
 
 X = T.fmatrix()
 Y = T.fmatrix()
 
-X = T.fmatrix()
-Y = T.fmatrix()
-
-w_h = init_weights((1089, 625))
+w_h = init_weights((trX.shape[1], 625))
 w_h2 = init_weights((625, 625))
-w_o = init_weights((625, 2))
+w_o = init_weights((625, num_class))
 
 noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
 h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
@@ -80,13 +88,15 @@ y_x = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
 params = [w_h, w_h2, w_o]
-updates = RMSprop(cost, params, lr=0.001)
+updates = RMSprop(cost, params, lr=lr)
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
-predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
+predict = theano.function(inputs=[X], outputs=py_x, allow_input_downcast=True)
 
-for i in range(300):
-    for start, end in zip(range(0, len(trX), 100), range(100, len(trX), 100)):
+for i in range(epos):
+    for start, end in zip(range(0, len(trX), min_batch), range(min_batch, len(trX), min_batch)):
         cost = train(trX[start:end], trY[start:end])
-    print np.mean(np.argmax(teY, axis=1) == predict(teX))
+    print np.mean(np.argmax(teY, axis=1) == np.argmax(predict(teX), axis=1))
 
+pred = predict(teX)
+np.savetxt("tmp.txt", pred)
