496c97523a9c82e92169076aedf3ffbd53fc8dc4
Rongxin Fang <r3fang@ucsd.edu>
2015-09-18 20:12:27 -0700

    5_convolutional_net.py

--- Teano/5_convolutional_net.py
+++ Teano/5_convolutional_net.py
@@ -20,7 +20,7 @@ def rectify(X):
 def softmax(X):
     e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
     return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
-    
+
 def dropout(X, p=0.):
     if p > 0:
         retain_prob = 1 - p
@@ -39,53 +39,54 @@ def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
         updates.append((acc, acc_new))
         updates.append((p, p - lr * g))
     return updates
-    
-def model(X, w, w2, w3, w4, p_drop_conv, p_drop_hidden):
-    l1a = rectify(conv2d(X, w, border_mode='full'))
+
+def model(X, w1, w2, w3, p_drop_conv, p_drop_hidden):
+    l1a = rectify(conv2d(X, w1, border_mode='full'))
     l1 = max_pool_2d(l1a, (2, 2))
     l1 = dropout(l1, p_drop_conv)
 
+    dropout(T.flatten(max_pool_2d(rectify(conv2d(X, w2)), (2,2)), outdim=2), 0.3)
+    
     l2a = rectify(conv2d(l1, w2))
-    l2 = max_pool_2d(l2a, (2, 2))
+    l2b = max_pool_2d(l2a, (2, 2))
+    l2 = T.flatten(l2b, outdim=2)
     l2 = dropout(l2, p_drop_conv)
 
-    l3a = rectify(conv2d(l2, w3))
-    l3b = max_pool_2d(l3a, (2, 2))
-    l3 = T.flatten(l3b, outdim=2)
-    l3 = dropout(l3, p_drop_conv)
-
-    l4 = rectify(T.dot(l3, w4))
-    l4 = dropout(l4, p_drop_hidden)
-
-    pyx = softmax(T.dot(l4, w_o))
-    return l1, l2, l3, l4, pyx
+    pyx = softmax(T.dot(l2, w3))
+    return l1, l2, pyx
 
 trX, teX, trY, teY = mnist(onehot=True)
 
 trX = trX.reshape(-1, 1, 28, 28)
 teX = teX.reshape(-1, 1, 28, 28)
 
+trX = trX[1:100]
+teX = teX[1:100]
+trY = trY[1:100]
+teY = teY[1:100]
+
+
 X = T.ftensor4()
 Y = T.fmatrix()
 
-w = init_weights((32, 1, 3, 3))
-w2 = init_weights((64, 32, 3, 3))
-w3 = init_weights((128, 64, 3, 3))
-w4 = init_weights((128 * 3 * 3, 625))
-w_o = init_weights((625, 10))
+w1 = init_weights((32, 1, 3, 3))
+w2 = init_weights((128, 32, 3, 3))
+w3 = init_weights((128 * 7 * 7, 10))
 
-noise_l1, noise_l2, noise_l3, noise_l4, noise_py_x = model(X, w, w2, w3, w4, 0.2, 0.5)
-l1, l2, l3, l4, py_x = model(X, w, w2, w3, w4, 0., 0.)
+noise_l1, noise_l2, noise_py_x = model(X, w1, w2, w3, 0.2, 0.5)
+l1, l2, py_x = model(X, w1, w2, w3, 0., 0.)
 y_x = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
-params = [w, w2, w3, w4, w_o]
+params = [w1, w2, w3]
 updates = RMSprop(cost, params, lr=0.001)
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
 for i in range(100):
-    for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
+    for start, end in zip(range(0, len(trX), 10), range(10, len(trX), 10)):
         cost = train(trX[start:end], trY[start:end])
+        print cost
     print np.mean(np.argmax(teY, axis=1) == predict(teX))
+
